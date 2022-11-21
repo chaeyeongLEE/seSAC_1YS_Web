ef86e49b054623e1786f38d227167f50554bb060
chaeyeongLEE <dh503401@naver.com>
2022-11-21 16:56:27 +0900

    node.js

---
+++ Node/Node.js
@@ -0,0 +1,21 @@
+var os = require('os');
+
+console.log('---os 모듈---')
+//const a = require('freemem');
+//const b = require('totalmem');
+//const c = require('homedir');
+
+//module.exports = {
+//    a, b, c
+//};
+console.log('os.hostname()==>' + os.hostname())
+console.log('os.freemem()==>' + os.freemem());
+console.log('os.totalmem()==>' + os.totalmem());
+console.log('os.homedir()==>' + os.homedir());
+
+ 
+//PS C:\Users\SBAUser\Desktop\github\seSAC_1YS_Web\nodejs\Node.js
+//__filename : C:\Users\SBAUser\Desktop\github\seSAC_1YS_Web\nodejs\Node.js
+//__dirname : C:\Users\SBAUser\Desktop\github\seSAC_1YS_Web\nodejs
+
+//exports = {os};
