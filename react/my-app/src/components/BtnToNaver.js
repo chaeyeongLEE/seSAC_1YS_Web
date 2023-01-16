12f9d9043719229eb2df9fe2f70b5bce2e99102f
chaeyeongLEE <dh503401@naver.com>
2023-01-17 01:09:20 +0900

    feat: html exercise

--- react/my-app/src/components/BtnToNaver.js
+++
@@ -1,9 +0,0 @@
-import { Component } from "react";
-
-class BtnToNaver extends Component {
-  render() {
-    return <a href="http://naver.com">네이버로 이동</a>;
-  }
-}
-
-export default BtnToNaver;
--- react/my-app/src/components/ImgComponent.js
+++
@@ -1,7 +0,0 @@
-import logo from "../logo.svg";
-
-function ImgComponent() {
-  return <img src={logo} alt="이미지" />;
-}
-
-export default ImgComponent;
